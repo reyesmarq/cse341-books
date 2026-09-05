5724f02d978a39fc5178c2a54ecb63be83f09284
Mauricio Reyes <maureyes.dev@gmail.com>
2026-09-05 11:32:22 -0600

    Add ESLint coding standards config and fix Express return violation

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello, world!' });
+  return res.status(200).json({ message: 'Hello, world!' });
 });
 
 export default app;
